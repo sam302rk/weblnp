6acaed97b92ac24ae5295a1ed33767420e6ea124
Samuel302 <samuel.mischl@gamingcraft.de>
2023-04-30 11:41:34 +0200

    Pipeline: Add license command + (BATCH) PyEnv Chooser * (BATCH) Fix unnessescery "" in echos

--- weblnp.bat
+++ weblnp.bat
@@ -2,21 +2,28 @@
 goto %1
 
 REM COMMAND NOT FOUND
-echo "Command unknown. Run 'weblnp help' for a list with commands."
+echo Command unknown. Run 'weblnp help' for a list with commands.
 exit
 
 Rem SERVE
 :serve
-echo "Starting local web server..."
-python -m http.server
+call :choose_python
+echo Starting local web server...
+%pyenv% -m http.server
 exit
 
 REM SORT
 :sort
-echo "Running sort script..."
+call :choose_python
+echo Running sort script...
 cd kml
-python sort.python
+%pyenv% sort.py
 cd ..
+exit
+
+REM LICENSE
+:license
+type LICENSE
 exit
 
 REM HELP
@@ -28,8 +35,12 @@ echo =========         =============
 echo weblnp help       Shows you a list with commands
 echo weblnp serve      Runs the local web server
 echo weblnp sort       Sorts KML files into city-folders
+echo weblnp license    Displays the repository's license.
 REM echo weblnp reload     Reloads the dev enviroment.
 REM echo weblnp exit       Exits out of the dev enviroment and disables the weblnp command
 echo.
-    
 exit
+
+:choose_python
+echo Enter a python enviroment of your choice [default: python]:
+set /p pyenv= || set pyenv=python
